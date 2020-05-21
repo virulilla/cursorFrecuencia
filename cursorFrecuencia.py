fc4cf78b9e6482c4bb207e502085e66cd5cee478
virulilla <e.salido.castro@gmail.com>
2020-05-21 08:58:31 +0200

    Se rellena el campo FRECUENCIA pero no se aplica el orden establecido en la clausula sql de los cursores utilizados

--- cursorFrecuencia.py
+++ cursorFrecuencia.py
@@ -1,16 +1,31 @@
 import arcpy, os
 arcpy.env.overwriteOutput = True
 
-
 dbf = "Stops_Tabla_seq_0_151.dbf"
 flag = False
 while not flag:
-    path = input("Ubicación de " + dbf + ": ")
+    path = input("Ubicacion de " + dbf + ": ")
     arcpy.env.workspace = path
     if arcpy.Exists(dbf):
         flag = True
-        arcpy.env.workspace = os.path.join(path, dbf)
     else:
         print("Este directorio no contiene el dbf")
 
-arcpy.Sort_management(dbf, "dbf_sorted", [["trip_id", "ASCENDING"], ["arrival_ti", "ASCENDING"]])
+# arcpy.Sort_management(dbf, "dbf_sorted", [["trip_id", "ASCENDING"], ["arrival_ti", "ASCENDING"]])
+
+# listField = ["OID", "OBJECTID", "trip_id", "arrival_ti", "departure", "stop_id", "stop_seque", "stop_heads", "pickup_typ", "drop_off_t", "shape_dist", "TIEMPO", "FRECUENCIA"]
+
+tiempoList = []
+i = 1
+with arcpy.da.SearchCursor(dbf, '*', sql_clause=(None, "ORDER BY trip_id, arrival_ti")) as sCursor:
+    for sRow in sCursor:
+        tiempoList.append(sRow[11])
+
+with arcpy.da.UpdateCursor(dbf, '*', sql_clause=(None, "ORDER BY trip_id, arrival_ti")) as uCursor:
+    for uRow in uCursor:
+        if i < len(tiempoList):
+            # uRow.next()
+            uRow[12] = tiempoList[i] - uRow[11]
+            uCursor.updateRow(uRow)
+            i += 1
+
